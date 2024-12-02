288b900b3221457e7c9d9baad5c8e743054e6717
Mark Keeble <markkeeble14@gmail.com>
2024-12-02 01:48:12 -0800

    d1 done

---
+++ Assets/Executable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+// Quicksort from here: https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-9.php
+
+public abstract class Executable : MonoBehaviour
+{
+    [SerializeField] private bool executeOnAwake = true;
+
+    private void Awake()
+    {
+        if (executeOnAwake)
+        {
+            Execute();
+        }
+    }
+
+    protected abstract void Execute();
+}
---
+++ Assets/TotalDistanceBetweenLists.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalDistanceBetweenLists : Executable
+{
+    [SerializeField] private TextAsset input;
+
+    [ContextMenu("Execute")]
+    protected override void Execute()
+    {
+        List<int> left = new List<int>();
+        List<int> right = new List<int>();
+
+        var splitfile = input.text.Split(new[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < splitfile.Length; i++)
+        {
+            if (i % 2 == 0)
+                left.Add(Int32.Parse(splitfile[i]));
+            else
+                right.Add(Int32.Parse(splitfile[i]));
+        }
+        int[] leftArr = left.ToArray();
+        int[] rightArr = right.ToArray();
+
+        Array.Sort(leftArr);
+        Array.Sort(rightArr);
+
+        int totalDistance = 0;
+        for (int i = 0; i < leftArr.Length; i++)
+        {
+            totalDistance += Mathf.Abs(leftArr[i] - rightArr[i]);
+        }
+
+        Debug.Log("The Total Distance between the Lists is: " + totalDistance);
+    }
+}
