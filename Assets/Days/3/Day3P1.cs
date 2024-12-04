2a765df92aa5e48681f3acff0690fc0c3168d80a
Mark Keeble <markkeeble14@gmail.com>
2024-12-03 23:14:26 -0800

    added day 3 part 1

--- Assets/Days/3/Day3P1.cs
+++ Assets/Days/3/Day3P1.cs
@@ -6,65 +6,130 @@ public class Day3P1 : Executable
 {
     protected override void Execute()
     {
-        bool capturingNum;
-        string num1Str;
+        string num;
         int num1;
-        string num2Str;
         int num2;
         char c;
         string str = "";
+        int total = 0;
+
         for (int i = 0; i < input.text.Length; i++)
-        {
-            c = input.text[i];
+        {            c = input.text[i];
             if (c == 'm')
             {
                 str += c;
                 continue;
             }
 
-            if (str == "m" && c == 'u')
+            if (str == "m")
             {
-                str += c;
-                continue;
-            }
-
-            if (str == "mu" && c == 'l')
-            {
-                str += c;
-                continue;
-            }
-
-            if (str == "mul" && c == '(')
-            {
-                str += c;
-
-                string num = "";
-                for (int p = i + 1; ; p++)
+                if (c == 'u')
                 {
-                    int c1;
-                    // if it's a number, add it to the string to be converted to a number later
-                    if (int.TryParse(input.text[p + 1].ToString(), out c1))
-                    {
-                        num += c1.ToString();
-                    } else if (input.text[p + 1] == ',') // if it's a comma, cool we do something
-                    {
-                        if (num.Length > 0)
-                        {
-                            num1 = int.Parse(num);
-                        }
-
-                    } else // if it's neither a number nor a comma, problem
-                    {
-                        str = "";
-                        break;
-                    }
+                    str += c;
+                    continue;
+                } else
+                {
+                    str = "";
+                    continue;
                 }
-
-
-                continue;
             }
 
+            if (str == "mu")
+            {
+                if (c == 'l')
+                {
+                    str += c;
+                    continue;
+                }
+                else
+                {
+                    str = "";
+                    continue;
+                }
+            }
 
+            if (str == "mul")
+            {
+                if (c == '(')
+                {
+                    str += c;
+
+                    // try to scan an int in
+                    num = ScanForInt(input.text, i + 1, ',');
+                    if (num == "")
+                    {
+                        str = ""; // failed to scan for some reason, reset and continue
+                        continue;
+                    }
+                    else
+                    {
+                        // succesfully scanned in num1
+                        num1 = int.Parse(num);
+
+                        // increment count by length of number + 1 (the comma)
+                        i += num.Length + 1;
+
+                        // try to scan another int in
+                        num = ScanForInt(input.text, i + 1, ')');
+
+                        if (num == "")
+                        {
+                            str = ""; // failed to scan for some reason, reset and continue
+                            continue;
+                        }
+                        else
+                        {
+                            // succesfully scanned in num2
+                            num2 = int.Parse(num);
+
+                            // increment count by length of number + 1 (the close bracket)
+                            i += num.Length + 1;
+
+                            // add to total
+                            total += num1 * num2;
+
+                            if (print)
+                                Debug.Log("Adding to Total: " + num1 + "x" + num2 + "=" + num1 * num2);
+
+                            str = "";
+                        }
+                    }
+                } else
+                {
+                    str = "";
+                    continue;
+                }
+            }
+        }
+
+        Debug.Log("Total: " + total);
+    }
+
+    private string ScanForInt(string s, int startIndex, char endScanOn)
+    {
+        string num = "";
+        int c1;
+        for (int p = startIndex; ; p++)
+        {
+            // if it's a number, add it to the string to be converted to a number later
+            if (int.TryParse(input.text[p].ToString(), out c1))
+            {
+                num += c1.ToString();
+            }
+            else if (input.text[p] == endScanOn) // if it's a comma, we make the number something
+            {
+                if (num.Length > 0)
+                {
+                    return num;
+                } else
+                {
+                    return "";
+                }
+            }
+            else // if it's neither a number nor a comma, problem
+            {
+                return "";
+            }
         }
     }
 }
